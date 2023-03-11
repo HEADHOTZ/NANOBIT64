d07593ba09ed2e041d7db89042de5093a1b737d8
Bunyakorn Ketkeaw <77223455+HEADHOTZ@users.noreply.github.com>
2023-03-11 17:51:58 +0700

    Delete NANOBIT TEST directory

--- NANOBIT TEST/EX10(Compass_result-led).py
+++
@@ -1,6 +0,0 @@
-from microbit import *
-while True:
-    angle = compass.heading()
-    display.scroll(str(angle))
-    print (angle)
-    sleep(100)
--- NANOBIT TEST/EX3.py
+++
@@ -1,9 +0,0 @@
-from microbit import *
-import tm1637
-Min = 10
-Sec = 30
-
-while True:
-    tm.number(Min, Sec)
-
-
