2a3bf732f2d87b854943584960fac8ab31e1399f
Bunyakorn Ketkeaw <77223455+HEADHOTZ@users.noreply.github.com>
2023-03-11 17:52:07 +0700

    Delete ทดลองเขียน directory

--- ทดลองเขียน/LED_RANDOM_IMAGE.py
+++
@@ -1,7 +0,0 @@
-from microbit import *
-import random
-image_list = [Image.HAPPY,Image.SAD,Image.HEART,Image.HEART_SMALL]
-
-while True:
-    if button_a.was_pressed():
-        display.show(random.choice(image_list))
